a511e44e76f070a198079db173358cf19aba818f
Mike van Oorschot <mike.vanoorschot@focus-solutions.co.uk>
2022-06-28 14:32:53 +0100

    Renamed classes/methods to be more self-explanatory and use an "explanatory method". Renamed test class/methods to read better, i.e. YearShould...BeALeapYearIf...

---
+++ LeapYear/Year.cs
@@ -0,0 +1,29 @@
+﻿namespace LeapYear;
+
+public class Year
+{
+    private readonly int _year;
+
+    public Year(int year)
+    {
+        _year = year;
+    }
+    
+    public bool IsLeap()
+    {
+        // not divisible by 4
+        if (!DivisibleBy(_year, 4)) return false;
+        
+        // divisible by 100 but not divisible by 400
+        if (DivisibleBy(_year, 100) && !DivisibleBy(_year, 400)) return false;
+        
+        // so at this point, year must be...
+        // divisible by 4 and, if divisible by 100, also divisible by 400
+        return true;
+    }
+
+    private bool DivisibleBy(int number, int divisor)
+    {
+        return (number % divisor) == 0;
+    }
+}
