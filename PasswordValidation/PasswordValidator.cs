2f58d3a1f073cd7e8f41eb13c2c7196123666bf2
Mike van Oorschot <mike.vanoorschot@focus-solutions.co.uk>
2022-07-08 10:24:16 +0100

    Initial commit. Completed iteration 1.

---
+++ PasswordValidation/PasswordValidator.cs
@@ -0,0 +1,39 @@
+﻿namespace PasswordValidation;
+
+public class PasswordValidator
+{
+    public bool Validate(string password)
+    {
+        // length
+        if (string.IsNullOrEmpty(password) || password.Length < 9)
+        {
+            return false;
+        }
+        
+        // uppercase
+        if (!password.Any(char.IsUpper))
+        {
+            return false;
+        }
+        
+        // lowercase
+        if (!password.Any(char.IsLower))
+        {
+            return false;
+        }
+        
+        // number
+        if (!password.Any(char.IsDigit))
+        {
+            return false;
+        }
+        
+        // underscore
+        if (password.IndexOf('_') < 0)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+}
