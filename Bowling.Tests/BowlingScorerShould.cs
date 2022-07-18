3f163cd1efe49f9f9a026ad1357ee79169d283be
Mike van Oorschot <mike.vanoorschot@focus-solutions.co.uk>
2022-07-18 15:20:18 +0100

    This version kind of works but does not correctly work out the score for a strike or spare. This is because it is difficult to find the next two or one (respectively) balls to add to the score. Thi sis partly because of the decision to create a class structure which contains a "Frame". Perhaps this is not needed and we can just have a list of Balls instead? Then it would be a simple matter to find the next two or one balls to calculate the score.

---
+++ Bowling.Tests/BowlingScorerShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Bowling.Tests;
+
+public class BowlingScorerShould
+{
+    [Theory]
+    [InlineAutoMoqData("X||", 10)]
+    [InlineAutoMoqData("5/||", 10)]
+    [InlineAutoMoqData("5-||", 5)]
+    [InlineAutoMoqData("-5||", 5)]
+    [InlineAutoMoqData("X|12||", 16)]
+    [InlineAutoMoqData("5/|12||", 14)]
+    [InlineAutoMoqData("X|X|X|X|X|X|X|X|X|X||XX", 300)]
+    [InlineAutoMoqData("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||", 90)]
+    [InlineAutoMoqData("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5", 150)]
+    [InlineAutoMoqData("X|7/|9-|X|-8|8/|-6|X|X|X||81", 167)]
+    public void CalculateAGameScore(string input, int expectedScore)
+    {
+        new BowlingScorer().Calculate(input).Should().Be(expectedScore);
+    }
+}
---
+++ Bowling/BowlingScorer.cs
@@ -0,0 +1,108 @@
+﻿namespace Bowling;
+
+public class BowlingScorer
+{
+    public int Calculate(string input)
+    {
+        // parse the game card
+        var parser = new GameCardParser();
+        var gameCard = parser.Parse(input);
+        
+        // calculate the number of pins for each ball
+        for (var index = 0; index < gameCard.Frames.Count; index++)
+        {
+            var frame = gameCard.Frames[index];
+            if (frame.Ball1.Code == 'X')
+            {
+                frame.Ball1.NumberOfPins = 10;
+                continue;
+            }
+            if (frame.Ball2.Code == '/')
+            {
+                frame.Ball1.NumberOfPins = ParseNumberOfPins(frame.Ball1.Code);
+                frame.Ball2.NumberOfPins = 10 - frame.Ball1.NumberOfPins;
+                continue;
+            }
+            if (frame.Ball1.Code != '-')
+            {
+                frame.Ball1.NumberOfPins = ParseNumberOfPins(frame.Ball1.Code);
+            }
+            if (frame.Ball2.Code != '-')
+            {
+                frame.Ball2.NumberOfPins = ParseNumberOfPins(frame.Ball2.Code);
+            }
+        }
+        
+        // calculate the bonus balls
+        if (gameCard.BonusBall1.Code == 'X')
+        {
+            gameCard.BonusBall1.NumberOfPins = 10;
+        }
+        else if (gameCard.BonusBall1.Code != '-')
+        {
+            gameCard.BonusBall1.NumberOfPins = ParseNumberOfPins(gameCard.BonusBall1.Code);
+        }
+        
+        if (gameCard.BonusBall2.Code == 'X')
+        {
+            gameCard.BonusBall2.NumberOfPins = 10;
+        }
+        else if (gameCard.BonusBall2.Code != '-')
+        {
+            gameCard.BonusBall2.NumberOfPins = ParseNumberOfPins(gameCard.BonusBall2.Code);
+        }
+
+        // score each frame
+        for (var index = 0; index < gameCard.Frames.Count; index++)
+        {
+            var frame = gameCard.Frames[index];
+            var nextFrame = new GameCardParser.Frame();
+            if (index + 1 < gameCard.Frames.Count)
+            {
+                nextFrame = gameCard.Frames[index + 1];
+            }
+
+            // strike
+            if (frame.Ball1.Code == 'X')
+            {
+                if (index == gameCard.Frames.Count)
+                {
+                    frame.Score = 10 + gameCard.BonusBall1.NumberOfPins + gameCard.BonusBall2.NumberOfPins;
+                }
+                else
+                {
+                    frame.Score = 10 + nextFrame.Ball1.NumberOfPins + nextFrame.Ball2.NumberOfPins;   
+                }
+            }
+            // spare
+            else if (frame.Ball2.Code == '/')
+            {
+                if (index == gameCard.Frames.Count)
+                {
+                    frame.Score = 10 + gameCard.BonusBall1.NumberOfPins;
+                }
+                else
+                {
+                    frame.Score = 10 + nextFrame.Ball1.NumberOfPins;
+                }
+            }
+            // normal
+            else
+            {
+                frame.Score = frame.Ball1.NumberOfPins + frame.Ball2.NumberOfPins;   
+            }
+        }
+
+        return gameCard.Frames.Sum(f => f.Score) + gameCard.BonusBall1.NumberOfPins + gameCard.BonusBall2.NumberOfPins;
+    }
+
+    private int ParseNumberOfPins(char? input)
+    {
+        if (!input.HasValue)
+        {
+            return 0;
+        }
+        
+        return int.Parse(input.ToString());
+    }
+}
