a8d857b2b8ea01a00fecbf108f382fbc252fe123
Mike van Oorschot <michael.oorschot@codurance.com>
2022-09-08 11:24:15 +0100

    Expanded ShoppingCart to support multiple products.

--- Csharp/ParallelAndNarrowChange/Field/ShoppingCart.cs
+++ Csharp/ParallelAndNarrowChange/Field/ShoppingCart.cs
@@ -1,22 +1,44 @@
-﻿namespace ParallelAndNarrowChange.Field
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ParallelAndNarrowChange.Field
 {
     public class ShoppingCart{
         private decimal price;
+        private List<int> prices = new List<int>();
 
         public decimal CalculateTotalPrice(){
             return price;
+        }
+        
+        public decimal CalculateTotalPriceMultiple()
+        {
+            return prices.Sum();
         }
 
         public bool HasDiscount(){
             return price > 100;
         }
+        
+        public bool HasDiscountMultiple()
+        {
+            return prices.Any(p => p > 100);
+        }
 
         public void Add(int aPrice){
-            this.price = aPrice;
+            price = aPrice;
+        }
+        
+        public void AddMultiple(params int[] prices){
+            this.prices.AddRange(prices);
         }
 
         public int NumberOfProducts(){
             return 1;
+        }
+        
+        public int NumberOfProductsMultiple(){
+            return this.prices.Count;
         }
     }
 }
--- Csharp/ParallelAndNarrowChange/Tests/ShoppingCartShould.cs
+++ Csharp/ParallelAndNarrowChange/Tests/ShoppingCartShould.cs
@@ -18,12 +18,26 @@ namespace ParallelAndNarrowChange{
 
             cart.CalculateTotalPrice().Should().Be(10);
         }
+        
+        [Test]
+        public void calculate_the_final_price_multiple(){
+            cart.AddMultiple(10, 20, 30);
+
+            cart.CalculateTotalPriceMultiple().Should().Be(60);
+        }
 
         [Test]
         public void knows_the_number_of_items(){
             cart.Add(10);
 
             cart.NumberOfProducts().Should().Be(1);
+        }
+        
+        [Test]
+        public void knows_the_number_of_items_multiple(){
+            cart.AddMultiple(10, 20, 30);
+
+            cart.NumberOfProductsMultiple().Should().Be(3);
         }
 
         [Test]
@@ -32,12 +46,26 @@ namespace ParallelAndNarrowChange{
 
             cart.HasDiscount().Should().BeTrue();
         }
+        
+        [Test]
+        public void may_offer_discounts_when_there_at_least_one_expensive_product_multiple(){
+            cart.AddMultiple(10, 20, 120);
+
+            cart.HasDiscountMultiple().Should().BeTrue();
+        }
 
         [Test]
         public void does_not_offer_discount_for_cheap_products(){
             cart.Add(10);
 
             cart.HasDiscount().Should().BeFalse();
+        }
+        
+        [Test]
+        public void does_not_offer_discount_for_cheap_products_multiple(){
+            cart.AddMultiple(10, 20, 30);
+
+            cart.HasDiscountMultiple().Should().BeFalse();
         }
     }
 }
