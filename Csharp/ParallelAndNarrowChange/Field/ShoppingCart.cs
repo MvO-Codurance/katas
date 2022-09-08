c7e686ce0f3588857f35e5224204b4c2c337c7aa
Mike van Oorschot <michael.oorschot@codurance.com>
2022-09-08 11:29:38 +0100

    Contracted code, removing old code and renaming new code to match old.

--- Csharp/ParallelAndNarrowChange/Field/ShoppingCart.cs
+++ Csharp/ParallelAndNarrowChange/Field/ShoppingCart.cs
@@ -4,41 +4,24 @@ using System.Linq;
 namespace ParallelAndNarrowChange.Field
 {
     public class ShoppingCart{
-        private decimal price;
         private List<int> prices = new List<int>();
 
-        public decimal CalculateTotalPrice(){
-            return price;
-        }
-        
-        public decimal CalculateTotalPriceMultiple()
+        public decimal CalculateTotalPrice()
         {
             return prices.Sum();
         }
-
-        public bool HasDiscount(){
-            return price > 100;
-        }
         
-        public bool HasDiscountMultiple()
+        public bool HasDiscount()
         {
             return prices.Any(p => p > 100);
         }
-
-        public void Add(int aPrice){
-            price = aPrice;
+        
+        public void Add(params int[] thePrices){
+            prices.AddRange(thePrices);
         }
         
-        public void AddMultiple(params int[] prices){
-            this.prices.AddRange(prices);
-        }
-
         public int NumberOfProducts(){
-            return 1;
-        }
-        
-        public int NumberOfProductsMultiple(){
-            return this.prices.Count;
+            return prices.Count;
         }
     }
 }
--- Csharp/ParallelAndNarrowChange/Tests/ShoppingCartShould.cs
+++ Csharp/ParallelAndNarrowChange/Tests/ShoppingCartShould.cs
@@ -14,58 +14,58 @@ namespace ParallelAndNarrowChange{
 
         [Test]
         public void calculate_the_final_price(){
-            cart.AddMultiple(10);
+            cart.Add(10);
 
-            cart.CalculateTotalPriceMultiple().Should().Be(10);
+            cart.CalculateTotalPrice().Should().Be(10);
         }
         
         [Test]
         public void calculate_the_final_price_multiple(){
-            cart.AddMultiple(10, 20, 30);
+            cart.Add(10, 20, 30);
 
-            cart.CalculateTotalPriceMultiple().Should().Be(60);
+            cart.CalculateTotalPrice().Should().Be(60);
         }
 
         [Test]
         public void knows_the_number_of_items(){
-            cart.AddMultiple(10);
+            cart.Add(10);
 
-            cart.NumberOfProductsMultiple().Should().Be(1);
+            cart.NumberOfProducts().Should().Be(1);
         }
         
         [Test]
         public void knows_the_number_of_items_multiple(){
-            cart.AddMultiple(10, 20, 30);
+            cart.Add(10, 20, 30);
 
-            cart.NumberOfProductsMultiple().Should().Be(3);
+            cart.NumberOfProducts().Should().Be(3);
         }
 
         [Test]
         public void may_offer_discounts_when_there_at_least_one_expensive_product(){
-            cart.AddMultiple(120);
+            cart.Add(120);
 
-            cart.HasDiscountMultiple().Should().BeTrue();
+            cart.HasDiscount().Should().BeTrue();
         }
         
         [Test]
         public void may_offer_discounts_when_there_at_least_one_expensive_product_multiple(){
-            cart.AddMultiple(10, 20, 120);
+            cart.Add(10, 20, 120);
 
-            cart.HasDiscountMultiple().Should().BeTrue();
+            cart.HasDiscount().Should().BeTrue();
         }
 
         [Test]
         public void does_not_offer_discount_for_cheap_products(){
-            cart.AddMultiple(10);
+            cart.Add(10);
 
-            cart.HasDiscountMultiple().Should().BeFalse();
+            cart.HasDiscount().Should().BeFalse();
         }
         
         [Test]
         public void does_not_offer_discount_for_cheap_products_multiple(){
-            cart.AddMultiple(10, 20, 30);
+            cart.Add(10, 20, 30);
 
-            cart.HasDiscountMultiple().Should().BeFalse();
+            cart.HasDiscount().Should().BeFalse();
         }
     }
 }
