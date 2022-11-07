3c920c381e6d0796ea342b93966f6d0ab875040f
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-07 14:49:15 +0000

    More refactoring.

--- Diamond/Diamond/DiamondPrinter.cs
+++ Diamond/Diamond/DiamondPrinter.cs
@@ -21,48 +21,36 @@ public class DiamondPrinter
             return "A";
         }
         
-        var result = new StringBuilder();
-        var indent = codeForInput - CodeForA;   // the indent is a count of space chars between the target letter and 'A'
-        var gap = 0;                            // the gap always starts at zero as 'A' is always output on it's own
-        var currentCode = CodeForA;             // we always start at 'A'
+        var lines =  BuildTopHalf(codeForInput);
+        lines.AddRange(BuildBottomHalf(lines));
+        
+        return string.Join(Environment.NewLine, lines);
+    }
 
-        // start at 'A' and loop until we reach the target letter
-        while (currentCode <= codeForInput)
+    private static List<string> BuildTopHalf(int targetLetterCode)
+    {
+        List<string> result = new();
+        var indent = targetLetterCode - CodeForA;   // the initial indent is the count of chars between the target letter and 'A'
+        var gap = 0;                                // the gap always starts at zero as 'A' is always output on it's own
+        var currentCode = CodeForA;                 // we always start at 'A'
+        
+        while (currentCode <= targetLetterCode)
         {
-            result.AppendLine(GetLine(currentCode, indent, gap));
+            result.Add(GetLine(currentCode, indent, gap));
             
             currentCode++;
-            indent--;               // the indent decreases by 1 for each line
-            gap = GetNextGap(gap);  // the gap increases in a pattern of odd numbers, i.e. 1, 3, 5, 7, ...
-        }
-        
-        // we've reached the target letter, the widest part of the diamond so now we reset
-        // we incremented past the target letter and we don't want to output the target letter again, so decrease by 2
-        currentCode -= 2;           
-        indent += 2;
-        gap -= 2;
-        gap = GetPreviousGap(gap);
-        
-        // starting at the target letter - 1, loop until we reach 'A' again
-        while (currentCode >= CodeForA)
-        {
-            var line = GetLine(currentCode, indent, gap);
-            
-            if (currentCode == CodeForA)
-            {
-                result.Append(line);
-            }
-            else
-            {
-                result.AppendLine(line);   
-            }
-            
-            currentCode--;
-            indent++;
-            gap = GetPreviousGap(gap);
+            indent--;
+            gap = GetNextGap(gap);
         }
 
-        return result.ToString();
+        return result;
+    }
+    
+    private static IEnumerable<string> BuildBottomHalf(IEnumerable<string> topHalf)
+    {
+        var bottomHalf = topHalf.ToList();
+        bottomHalf.Reverse();
+        return bottomHalf.Skip(1);
     }
 
     private static string GetLine(int letterCode, int indent, int gap)
@@ -83,10 +71,5 @@ public class DiamondPrinter
     private static int GetNextGap(int gap)
     {
         return gap == 0 ? gap + 1 : gap + 2;
-    }
-
-    private static int GetPreviousGap(int gap)
-    {
-        return gap == 1 ? gap - 1 : gap - 2;
     }
 }
