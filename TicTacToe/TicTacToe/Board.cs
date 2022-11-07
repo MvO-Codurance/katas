79a4a33cd5a4e20abd8050e6d56dd22e21d3e48d
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-07 16:07:07 +0000

    Refactored more board-related methods into Board.cs.

--- TicTacToe/TicTacToe/Board.cs
+++ TicTacToe/TicTacToe/Board.cs
@@ -23,4 +23,70 @@ public class Board
     {
         _board[coordinate.X, coordinate.Y] = player;
     }
+
+    public bool HasSomebodyWon()
+    {
+        return ColumnWin() || RowWin() || BottomLeftTopRightWin() || TopLeftBottomRightWin();
+    }
+    
+    public bool IsDraw()
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                if (!HasSquareBeenPlayed(new Coordinate(x, y)))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    
+    private bool ColumnWin()
+    {
+        for (int columnIndex = 0; columnIndex < 3; columnIndex++)
+        {
+            if (IsWinningSequence(new Coordinate(columnIndex, 0), new Coordinate(columnIndex, 1), new Coordinate(columnIndex, 2)))
+            {
+                return true;
+            }    
+        }
+
+        return false;
+    }
+
+    private bool RowWin()
+    {
+        for (int rowIndex = 0; rowIndex < 3; rowIndex++)
+        {
+            if (IsWinningSequence(new Coordinate(0, rowIndex), new Coordinate(1, rowIndex), new Coordinate(2, rowIndex)))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool BottomLeftTopRightWin()
+    {
+        return IsWinningSequence(new Coordinate(0, 0), new Coordinate(1, 1), new Coordinate(2, 2));
+    }
+
+    private bool TopLeftBottomRightWin()
+    {
+        return IsWinningSequence(new Coordinate(0, 2), new Coordinate(1, 1), new Coordinate(2, 0));
+    }
+
+    private bool IsWinningSequence(Coordinate square1, Coordinate square2, Coordinate square3)
+    {
+        return HasSquareBeenPlayed(square1) && 
+               HasSquareBeenPlayed(square2) &&
+               HasSquareBeenPlayed(square3) &&
+               (Square(square1)!.Value == Square(square2)!.Value &&
+                Square(square2)!.Value == Square(square3)!.Value);
+    }
 }
--- TicTacToe/TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -28,13 +28,13 @@ public class TicTacToeGame
         
         Board.PlaySquare(coordinate, CurrentPlayer);
 
-        if (ColumnWin() || RowWin() || BottomLeftTopRightWin() || TopLeftBottomRightWin())
+        if (Board.HasSomebodyWon())
         {
             _lastPlayResult = CurrentPlayer == Player.X ? PlayResult.PlayerXWins : PlayResult.PlayerOWins;
             return _lastPlayResult;
         }
         
-        if (IsDraw())
+        if (Board.IsDraw())
         {
             _lastPlayResult = PlayResult.Draw;
             return _lastPlayResult;
@@ -51,66 +51,5 @@ public class TicTacToeGame
     private bool HasGameAlreadyFinished()
     {
         return _lastPlayResult is PlayResult.Draw or PlayResult.PlayerXWins or PlayResult.PlayerOWins;
-    }
-
-    private bool ColumnWin()
-    {
-        for (int columnIndex = 0; columnIndex < 3; columnIndex++)
-        {
-            if (IsWinningSequence(new Coordinate(columnIndex, 0), new Coordinate(columnIndex, 1), new Coordinate(columnIndex, 2)))
-            {
-                return true;
-            }    
-        }
-
-        return false;
-    }
-
-    private bool RowWin()
-    {
-        for (int rowIndex = 0; rowIndex < 3; rowIndex++)
-        {
-            if (IsWinningSequence(new Coordinate(0, rowIndex), new Coordinate(1, rowIndex), new Coordinate(2, rowIndex)))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private bool BottomLeftTopRightWin()
-    {
-        return IsWinningSequence(new Coordinate(0, 0), new Coordinate(1, 1), new Coordinate(2, 2));
-    }
-
-    private bool TopLeftBottomRightWin()
-    {
-        return IsWinningSequence(new Coordinate(0, 2), new Coordinate(1, 1), new Coordinate(2, 0));
-    }
-
-    private bool IsWinningSequence(Coordinate square1, Coordinate square2, Coordinate square3)
-    {
-        return Board.HasSquareBeenPlayed(square1) && 
-               Board.HasSquareBeenPlayed(square2) &&
-               Board.HasSquareBeenPlayed(square3) &&
-               (Board.Square(square1)!.Value == Board.Square(square2)!.Value &&
-                Board.Square(square2)!.Value == Board.Square(square3)!.Value);
-    }
-
-    private bool IsDraw()
-    {
-        for (int x = 0; x < 3; x++)
-        {
-            for (int y = 0; y < 3; y++)
-            {
-                if (!Board.HasSquareBeenPlayed(new Coordinate(x, y)))
-                {
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 }
