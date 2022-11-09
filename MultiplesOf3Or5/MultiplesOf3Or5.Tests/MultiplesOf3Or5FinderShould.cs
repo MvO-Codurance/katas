ecefdc53e5495ed0f6407cf3ed45541b6078621b
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-09 12:16:30 +0000

    Added algorithmic solution.

--- MultiplesOf3Or5/MultiplesOf3Or5.Tests/MultiplesOf3Or5FinderShould.cs
+++ MultiplesOf3Or5/MultiplesOf3Or5.Tests/MultiplesOf3Or5FinderShould.cs
@@ -6,17 +6,32 @@ namespace MultiplesOf3Or5.Tests;
 public class MultiplesOf3Or5FinderShould
 {
     [Fact]
-    public void Calculate_The_Sum_Of_Multiples_Of_3_Or_5_Below_10_To_Be_23()
+    public void Calculate_The_SimpleSum_Of_Multiples_Of_3_Or_5_Below_10_To_Be_23()
     {
-        new MultiplesOf3Or5Finder().Sum(10).Should().Be(23);
+        new MultiplesOf3Or5Finder().SimpleSum(10).Should().Be(23);
     }
     
     [Theory]
     [InlineAutoNSubstituteData(10, 23)]
     [InlineAutoNSubstituteData(20, 78)]
     [InlineAutoNSubstituteData(1000, 233168)]
-    public void Correctly_Calculate_The_Sum_Of_Multiples_Of_3_Or_5_Below_The_Limit(int limit, int expectedSum)
+    public void Correctly_Calculate_The_SimpleSum_Of_Multiples_Of_3_Or_5_Below_The_Limit(int limit, int expectedSum)
     {
-        new MultiplesOf3Or5Finder().Sum(limit).Should().Be(expectedSum);
+        new MultiplesOf3Or5Finder().SimpleSum(limit).Should().Be(expectedSum);
+    }
+    
+    [Fact]
+    public void Calculate_The_AlgorithmicSum_Of_Multiples_Of_3_Or_5_Below_10_To_Be_23()
+    {
+        new MultiplesOf3Or5Finder().AlgorithmicSum(10).Should().Be(23);
+    }
+    
+    [Theory]
+    [InlineAutoNSubstituteData(10, 23)]
+    [InlineAutoNSubstituteData(20, 78)]
+    [InlineAutoNSubstituteData(1000, 233168)]
+    public void Correctly_Calculate_The_AlgorithmicSum_Of_Multiples_Of_3_Or_5_Below_The_Limit(int limit, int expectedSum)
+    {
+        new MultiplesOf3Or5Finder().AlgorithmicSum(limit).Should().Be(expectedSum);
     }
 }
--- MultiplesOf3Or5/MultiplesOf3Or5/MultiplesOf3Or5Finder.cs
+++ MultiplesOf3Or5/MultiplesOf3Or5/MultiplesOf3Or5Finder.cs
@@ -2,7 +2,7 @@ namespace MultiplesOf3Or5;
 
 public class MultiplesOf3Or5Finder
 {
-    public int Sum(int limit)
+    public int SimpleSum(int limit)
     {
         int sum = 0;
         
@@ -15,5 +15,35 @@ public class MultiplesOf3Or5Finder
         }
         
         return sum;
+    }
+
+    public int AlgorithmicSum(int limit)
+    {
+        /* The sum of of all numbers between 1 and n is:
+         * n * (n + 1) / 2
+         *
+         * Also, the sum of numbers LESS THAN n that are divisible by d is:
+         * d * ( (n/d) * ((n/d) + 1) ) / 2
+         *
+         * So, for numbers below 10 (i.e. 9):
+         * Numbers divisible by 3 is: 3 * ( (9/3) * ((9/3) + 1) ) / 2
+         * Numbers divisible by 5 is: 5 * ( (9/5) * ((9/5) + 1) ) / 2
+         *
+         * But the above sets will double count numbers that are divisible by BOTH 3 and 5.
+         * So we then subtract from the above the set of numbers that are divisible by 15, i.e.:
+         * 15 * ( (9/15) * ((9/15) + 1) ) / 2
+         */
+
+        int n = limit - 1;
+        int divisibleBy3 = SumOfNumbersDivisibleBy(n, 3);
+        int divisibleBy5 = SumOfNumbersDivisibleBy(n, 5);
+        int divisibleBy15 = SumOfNumbersDivisibleBy(n, 15);
+
+        return divisibleBy3 + divisibleBy5 - divisibleBy15;
+    }
+
+    private static int SumOfNumbersDivisibleBy(int n, int d)
+    {
+        return d * ((n / d) * ((n / d) + 1)) / 2;
     }
 }
