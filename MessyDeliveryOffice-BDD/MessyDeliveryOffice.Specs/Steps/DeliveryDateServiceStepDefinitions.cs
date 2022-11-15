d3a1bd6cdecca579c7d340903e37b544e49e50d6
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-15 13:31:06 +0000

    Added unit test project. Updated all projects to net7.0 Added skeleton implementations for DeliveryDateService feature.

--- MessyDeliveryOffice-BDD/MessyDeliveryOffice.Specs/Steps/DeliveryDateServiceStepDefinitions.cs
+++ MessyDeliveryOffice-BDD/MessyDeliveryOffice.Specs/Steps/DeliveryDateServiceStepDefinitions.cs
@@ -3,5 +3,93 @@ namespace MessyDeliveryOffice.Specs.Steps;
 [Binding]
 public class DeliveryDateServiceStepDefinitions
 {
-    
+    [Given(@"the id of a customer order")]
+    public void GivenTheIdOfACustomerOrder()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"we ask for the estimated dispatch date for the order")]
+    public void WhenWeAskForTheEstimatedDispatchDateForTheOrder()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"the result should be the estimated dispatch date for the order")]
+    public void ThenTheResultShouldBeTheEstimatedDispatchDateForTheOrder()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"the order is received by the supplier")]
+    public void WhenTheOrderIsReceivedByTheSupplier()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"the supplier starts to process the order on the same day")]
+    public void ThenTheSupplierStartsToProcessTheOrderOnTheSameDay()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"a product that the supplier supplies that has a lead time of (.*) days")]
+    public void GivenAProductThatTheSupplierSuppliesThatHasALeadTimeOfDays(string p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"the order received date plus (.*) days falls on a week day")]
+    public void GivenTheOrderReceivedDatePlusDaysFallsOnAWeekDay(string p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"the product is sent to the delivery office (.*) days later")]
+    public void ThenTheProductIsSentToTheDeliveryOfficeDaysLater(string p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"the order received date plus (.*) days falls on a weekend")]
+    public void GivenTheOrderReceivedDatePlusDaysFallsOnAWeekend(string p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"the product is sent to the delivery office on the next weekday plus the remaining (.*) days")]
+    public void ThenTheProductIsSentToTheDeliveryOfficeOnTheNextWeekdayPlusTheRemainingDays(string p0)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"the day of the week is a weekday")]
+    public void GivenTheDayOfTheWeekIsAWeekday()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"all products on the order have been received by the Delivery Office")]
+    public void WhenAllProductsOnTheOrderHaveBeenReceivedByTheDeliveryOffice()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"the order is dispatched to the customer on the same day")]
+    public void ThenTheOrderIsDispatchedToTheCustomerOnTheSameDay()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"the day of the week is a weekend")]
+    public void GivenTheDayOfTheWeekIsAWeekend()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"the order is dispatched to the customer on the next weekday")]
+    public void ThenTheOrderIsDispatchedToTheCustomerOnTheNextWeekday()
+    {
+        ScenarioContext.StepIsPending();
+    }
 }
---
+++ MessyDeliveryOffice-BDD/MessyDeliveryOffice.Tests/InlineAutoNSubstituteDataAttribute.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+
+namespace MessyDeliveryOffice.Tests
+{
+    /// <summary>
+    /// Inline auto NSubstitute data attribute to allow for inline auto fixture test models as well as NSubstitute based items
+    /// </summary>
+    public class InlineAutoNSubstituteDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoNSubstituteDataAttribute(params object[] values)
+            : base(new AutoNSubstituteDataAttribute(), values)
+        {
+        }
+
+        /// <summary>
+        /// Private attribute which is required to be passed in. This sets up the <see cref="IFixture"/> to hook NSubstitute into the pipeline.
+        /// </summary>
+        private class AutoNSubstituteDataAttribute : AutoDataAttribute
+        {
+            public AutoNSubstituteDataAttribute()
+                : base(() =>
+                {
+                    var fixture = new Fixture();
+                    fixture.Customize(new CompositeCustomization(new AutoNSubstituteCustomization()));
+                    return fixture;
+                })
+            {
+            }
+        }
+    }
+}
